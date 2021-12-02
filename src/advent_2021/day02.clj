b7652a452381ebb030780aa3ef67198299b00ac3
Thomas Sojka <thsojka@web.de>
2021-12-02 07:41:37 +0100

    Add day02

---
+++ src/advent_2021/day02.clj
@@ -0,0 +1,40 @@
+(ns advent-2021.day02
+  (:require [advent-2021.utils :refer [read-file]]))
+
+(defn prep [data]
+  (->> data
+       (map #(str/split % #" "))
+       (map (fn [[command amount]] [command (Integer/parseInt amount)]))))
+
+(defn part-1 [data]
+  (let [[pos depth]
+        (reduce (fn [[pos depth] [command amount]]
+                  (case command
+                    "forward"
+                    [(+ pos amount) depth]
+                    "up"
+                    [pos (- depth amount)]
+                    "down"
+                    [pos (+ depth amount)]))
+                [0 0]
+                (prep data))]
+    (* pos depth)))
+
+(defn part-2 [data]
+  (let [[pos depth]
+        (reduce (fn [[pos depth aim] [command amount]]
+                  (case command
+                    "down"
+                    [pos depth (+ aim amount)]
+                    "up"
+                    [pos depth (- aim amount)]
+                    "forward"
+                    [(+ pos amount) (+ depth (* aim amount)) aim]))
+                [0 0 0]
+                (prep data))]
+    (* pos depth)))
+
+(comment
+  (part-1 (read-file "day2.txt"))
+  (part-2 (read-file "day2.txt")))
+
---
+++ src/advent_2021/utils.clj
@@ -0,0 +1,7 @@
+(ns advent-2021.utils
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+(defn read-file [file-path]
+  (str/split-lines (slurp (io/resource "day2.txt"))))

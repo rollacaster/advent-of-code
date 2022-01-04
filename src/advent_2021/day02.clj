01295be03cd96fed046c27c5353de5a0cc4e8865
Thomas Sojka <thsojka@web.de>
2022-01-04 08:28:35 +0100

    Refactor day 2

--- src/advent_2021/day02.clj
+++ src/advent_2021/day02.clj
@@ -1,41 +1,36 @@
 (ns advent-2021.day02
-  (:require [advent-2021.utils :refer [read-file]]
-            [clojure.string :as str]))
+  (:require [clojure.java.io :as io]))
 
-(defn prep [data]
-  (->> data
-       (map #(str/split % #" "))
-       (map (fn [[command amount]] [command (Integer/parseInt amount)]))))
+(defn parse-input [input]
+  (->> input
+       (re-seq #"[\d\w]+")
+       (partition 2)
+       (map (fn [[c v]] [c (parse-long v)]))))
 
-(defn part-1 [data]
-  (let [[pos depth]
-        (reduce (fn [[pos depth] [command amount]]
-                  (case command
-                    "forward"
-                    [(+ pos amount) depth]
-                    "up"
-                    [pos (- depth amount)]
-                    "down"
-                    [pos (+ depth amount)]))
-                [0 0]
-                (prep data))]
-    (* pos depth)))
+(defn part-1 [input]
+  (->> (parse-input input)
+       (reduce (fn [[pos depth] [command amount]]
+                 (case command
+                   "forward" [(+ pos amount) depth]
+                   "up" [pos (- depth amount)]
+                   "down" [pos (+ depth amount)]))
+               [0 0])
+       (apply *)))
 
-(defn part-2 [data]
-  (let [[pos depth]
-        (reduce (fn [[pos depth aim] [command amount]]
-                  (case command
-                    "down"
-                    [pos depth (+ aim amount)]
-                    "up"
-                    [pos depth (- aim amount)]
-                    "forward"
-                    [(+ pos amount) (+ depth (* aim amount)) aim]))
-                [0 0 0]
-                (prep data))]
-    (* pos depth)))
+(defn part-2 [input]
+  (->> (parse-input input)
+       (reduce (fn [[pos depth aim] [command amount]]
+                 (case command
+                   "down" [pos depth (+ aim amount)]
+                   "up" [pos depth (- aim amount)]
+                   "forward" [(+ pos amount) (+ depth (* aim amount)) aim]))
+               [0 0 0])
+       drop-last
+       (apply *)))
 
 (comment
-  (part-1 (read-file "day2.txt"))
-  (part-2 (read-file "day2.txt")))
+  (part-1 (slurp (io/resource "day02.txt")))
+  (part-2 (slurp (io/resource "day02.txt"))))
+
+
 

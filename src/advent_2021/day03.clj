376fe2506b64492e92855bb5ecbac4f0ab940eef
Thomas Sojka <thsojka@web.de>
2022-01-08 15:02:40 +0100

    Refactor day 3

--- src/advent_2021/day03.clj
+++ src/advent_2021/day03.clj
@@ -1,73 +1,62 @@
 (ns advent-2021.day03
   (:require
-   [advent-2021.utils :refer [read-file]]
+   [clojure.java.io :as io]
    [clojure.string :as str]))
-
-(def input
-  ["00100"
-   "11110"
-   "10110"
-   "10111"
-   "10101"
-   "01111"
-   "00111"
-   "11100"
-   "10000"
-   "11001"
-   "00010"
-   "01010"])
-
-(defn binary [decimal]
-  (if (= decimal 0)
-    0
-    (+ (mod decimal 2) (* 10 (binary (quot decimal 2))))))
 
 (defn read-binary [binary-string]
   (read-string (str "2r" binary-string)))
 
-(defn group-occurences [input]
-  (apply map
-         (fn [& args]
-           (->> args
-                (group-by identity)
-                (map (fn [[n ns]] [n (count ns)]))))
-         input))
+(defn parse-input [input]
+  (str/split-lines input))
 
 (defn part1 [input]
-  (let [gamma
-        (->> input
-             group-occurences
-             (map (fn [[[n1 c1] [n2 c2]]] (if (> c1 c2) n1 n2)))
-             str/join)
-        epsilon (->> gamma
-                     (map (fn [i] (if (= i \0) \1 \0)))
-                     (apply str))]
-    (* (read-binary gamma) (read-binary epsilon))))
+  (->> (parse-input input)
+       (apply map vector)
+       (map (comp
+             #(map first %)
+             #(sort-by second %)
+             vec
+             frequencies))
+       (apply map vector)
+       (map (comp read-binary
+                  #(apply str %)))
+       (apply *)))
 
-(defn c [f pos input]
-  (if (= (count input) 1)
-    (first input)
-    (c f (inc pos) (filter #(= (nth % pos) (nth (map f (group-occurences input)) pos)) input))))
+(defn bit-criteria [rating-type idx [a b]]
+  (cond
+    (= (count a) (count b))
+    (filter
+     #(= (nth % idx) (if (= rating-type :oxy) \1 \0))
+     (concat a b))
+    ((if (= rating-type :oxy) > <) (count a) (count b))
+    a
+    :else
+    b))
+
+(defn matching-numbers [type idx numbers]
+  (->> numbers
+       (group-by #(nth % idx))
+       (map second)
+       (bit-criteria type idx)))
+
+(defn find-rating [type numbers]
+  (loop [numbers numbers
+         idx 0]
+    (let [numbers (matching-numbers type idx numbers)]
+      (if (= (count numbers) 1)
+        (first numbers)
+        (recur numbers (inc idx))))))
 
 (defn part2 [input]
-  (->> [(fn [[[n1 c1] [n2 c2]]]
-          (cond
-            (not c2) n1 
-            (= c1 c2) \0
-            (> c1 c2) n2
-            :else n1))
-        (fn [[[n1 c1] [n2 c2]]]
-          (cond
-            (not c2) n1 
-            (= c1 c2) \1
-            (> c1 c2) n1
-            :else n2))]
-       (map (fn [f] (read-binary ((partial c f 0) (read-file "day3.txt")))))
+  (->> (parse-input input)
+       ((juxt (partial find-rating :oxy)
+              (partial find-rating :scrub)))
+       (map (comp read-binary parse-long))
        (apply *)))
 
 (comment
-  (part1 (read-file "day3.txt"))
-  (part2 (read-file "day3.txt")))
+  (part1 (slurp (io/resource "day3.txt")))
+  (part2 (slurp (io/resource "day3.txt"))))
 
 
 

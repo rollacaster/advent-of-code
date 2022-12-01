372020216c7ad4323a34387be8e444c3f6d6cd3e
Thomas Sojka <thsojka@web.de>
2022-12-01 07:52:32 +0100

    Setup

--- 2022/dev/user.clj
+++ 2022/dev/user.clj
@@ -2,8 +2,7 @@
   (:require [nextjournal.clerk :as clerk]
             [advent-of-clerk.index :as index]))
 
-(clerk/serve! {:port 7878 :browse true})
+(clerk/serve! {:port 7878 :browse true :watch-paths ["src"]})
 
 (comment
   (clerk/build! {:paths (index/build-paths) :browse true}))
-

ef8f886e470be387ee522244708af28715d64c21
Thomas Sojka <thsojka@web.de>
2022-12-01 07:47:29 +0100

    Day01

--- 2022/src/advent_of_clerk/day_01.clj
+++ 2022/src/advent_of_clerk/day_01.clj
@@ -1,3 +1,21 @@
 ;; # 🎄 Advent of Clerk: Day 1
 (ns advent-of-clerk.day-01
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.string :as str]))
+
+(def input (slurp "resources/day01.txt"))
+
+(defn part1 [input]
+  (->> (str/split input #"\n\n")
+       (map #(->> % (re-seq #"\d*") (keep parse-long) (reduce +)))
+       (apply max)))
+
+(part1 input)
+
+(defn part2 [input]
+  (->> (str/split input #"\n\n")
+       (map #(->> % (re-seq #"\d*") (keep parse-long) (reduce +)))
+       (sort >)
+       (take 3)
+       (reduce +)))
+
+(part2 input)

7280858bb00c4e324e550e226d4fdfe55ef1cdef
Thomas Sojka <thsojka@web.de>
2022-12-02 07:28:15 +0100

    Day02

--- 2022/src/advent_of_clerk/day_02.clj
+++ 2022/src/advent_of_clerk/day_02.clj
@@ -1,3 +1,47 @@
 ;; # 🎄 Advent of Clerk: Day 2
 (ns advent-of-clerk.day-02
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.string :as str]))
+
+(def input (slurp "resources/day02.txt"))
+
+(def shape-score {"X" 1
+                  "Y" 2
+                  "Z" 3
+                  ["A" "X"] 3
+                  ["B" "X"] 1
+                  ["C" "X"] 2
+                  ["A" "Y"] 1
+                  ["B" "Y"] 2
+                  ["C" "Y"] 3
+                  ["A" "Z"] 2
+                  ["B" "Z"] 3
+                  ["C" "Z"] 1})
+
+(def outcome-score {["A" "X"] 3
+                    ["B" "X"] 0
+                    ["C" "X"] 6
+                    ["A" "Y"] 6
+                    ["B" "Y"] 3
+                    ["C" "Y"] 0
+                    ["A" "Z"] 0
+                    ["B" "Z"] 6
+                    ["C" "Z"] 3
+                    "X" 0
+                    "Y" 3
+                    "Z" 6})
+
+(defn parse-input [input]
+  (mapv #(re-seq #"\w" %) (str/split-lines input)))
+
+(defn result [f]
+  (->> input parse-input (reduce f 0)))
+
+(defn part1 [sum [_ yours :as game]]
+  (+ sum (shape-score yours) (outcome-score game)))
+
+(result part1)
+
+(defn part2 [sum [_ result :as game]]
+  (+ sum (shape-score game) (outcome-score result)))
+
+(result part2)

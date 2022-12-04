794a7c7d9939dd0a09d856ae944d02b77a0f875a
Thomas Sojka <thsojka@web.de>
2022-12-04 19:25:14 +0100

    Day03

--- 2022/src/advent_of_clerk/day_03.clj
+++ 2022/src/advent_of_clerk/day_03.clj
@@ -1,3 +1,43 @@
 ;; # 🎄 Advent of Clerk: Day 3
 (ns advent-of-clerk.day-03
-  (:require [nextjournal.clerk :as clerk]))
+  (:require
+   [clojure.set :as set]
+   [clojure.string :as str]))
+
+(def input (slurp "resources/day03.txt"))
+
+(defn letter->int [letter-int]
+  (cond
+    (<= 65 letter-int 90) (- letter-int 38)
+    (<= 97 letter-int 122) (- letter-int 96)))
+
+(defn- bag [bag-line]
+  (->> bag-line
+       (map (comp letter->int int))
+       (split-at (/ (count bag-line) 2))
+       (map set)
+       (apply set/intersection)
+       first))
+
+(defn part1 [input]
+  (->> input
+       str/split-lines
+       (mapv bag)
+       (reduce +)))
+
+(defn- bag-group [bag-lines]
+  (->> bag-lines
+       (map #(set (map (comp letter->int int) %)))
+       (apply set/intersection)
+       first))
+
+(defn part2 [input]
+  (->> input
+       str/split-lines
+       (partition 3)
+       (map bag-group)
+       (reduce +)))
+
+(part1 input)
+
+(part2 input)

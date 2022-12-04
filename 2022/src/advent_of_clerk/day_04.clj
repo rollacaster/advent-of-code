55def021e63477a5ae424a7e6fa8864e8f5f14c6
Thomas Sojka <thsojka@web.de>
2022-12-04 19:26:53 +0100

    Day04

--- 2022/src/advent_of_clerk/day_04.clj
+++ 2022/src/advent_of_clerk/day_04.clj
@@ -1,3 +1,31 @@
 ;; # 🎄 Advent of Clerk: Day 4
 (ns advent-of-clerk.day-04
-  (:require [nextjournal.clerk :as clerk]))
+  (:require
+   [clojure.set :as set]
+   [clojure.string :as str]))
+
+(def input (slurp "resources/day04.txt"))
+
+(defn ->range [line]
+  (->> (str/split line #",")
+       (mapcat #(str/split % #"-"))
+       (map parse-long)
+       (partition 2)
+       (map (fn [[a b]] (set (range a (inc b)))))))
+
+(defn part1 [input]
+  (->> input
+       str/split-lines
+       (map ->range)
+       (filter (fn [[a b]] (or (set/subset? a b) (set/subset? b a))))
+       count))
+
+(defn part2 [input]
+  (->> input
+       str/split-lines
+       (map ->range)
+       (filter (fn [[a b]] (seq (set/intersection a b))))
+       count))
+
+(part1 input)
+(part2 input)

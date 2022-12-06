15ef407fe64b7c6f53d8fb0b014f64cab50408c9
Thomas Sojka <thsojka@web.de>
2022-12-06 07:05:52 +0100

    Day06

--- 2022/src/advent_of_clerk/day_06.clj
+++ 2022/src/advent_of_clerk/day_06.clj
@@ -1,3 +1,21 @@
 ;; # 🎄 Advent of Clerk: Day 6
-(ns advent-of-clerk.day-06
-  (:require [nextjournal.clerk :as clerk]))
+(ns advent-of-clerk.day-06)
+
+(def input (slurp "resources/day06.txt"))
+
+(defn run [input length]
+  (->> input
+       (partition length 1)
+       (take-while #(not (apply distinct? %)))
+       count
+       (+ length)))
+
+(defn part1 [input]
+  (run input 4))
+
+(defn part2 [input]
+  (run input 14))
+
+(part1 input)
+
+(part2 input)

55b7f739c19ee08e8f5d371255c0c6d64ef28779
Thomas Sojka <thsojka@web.de>
2022-12-10 10:48:46 +0100

    Day10

--- 2022/src/advent_of_clerk/day_10.clj
+++ 2022/src/advent_of_clerk/day_10.clj
@@ -1,3 +1,39 @@
 ;; # 🎄 Advent of Clerk: Day 10
 (ns advent-of-clerk.day-10
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.string :as str]))
+
+(def input (slurp "resources/day10.txt"))
+
+(defn parse-input [input]
+  (mapcat (fn [s] (if (str/includes? s "noo")
+                   [[:noop]]
+                   [[:noop] [:addx (parse-long (re-find #"-?\d+" s))]]))
+          (str/split-lines input)))
+
+(defn- step [state [command value]]
+  (-> state
+      (update 0 inc)
+      (update 1 (fn [v] (if (= command :addx) (+ value v) v)))))
+
+(defn steps [input]
+  (->> input
+       parse-input
+       (reductions step [1 1])))
+
+(defn part1 [input]
+  (->> [20 60 100 140 180 220]
+       (map (fn [i] (apply * (nth (steps input) (dec i)))))
+       (apply +)))
+
+(part1 input)
+
+(defn part2 [input]
+  (->> (steps input)
+       (reduce
+        (fn [screen [cycle pos]]
+          (str screen (if (<= pos (mod cycle 40) (-> pos inc inc)) "#" ".")))
+        "")
+       (partition 40)
+       (map #(apply str %))))
+
+(part2 input)
